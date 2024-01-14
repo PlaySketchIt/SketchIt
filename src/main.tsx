57ea3e29e5351e2de2c642c74128c3bd19088b08
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-14 20:28:15 +0000

    variable font + weight

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-import "typeface-open-sans";
+import "@fontsource-variable/open-sans";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
