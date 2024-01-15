33843130553dd9bf9da05f53503739020f7e6489
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-15 18:16:47 +0000

    fix exclusion rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
       { allowConstantExport: true },
     ],
     "quotes": ["error", "double"],
-    "@typescript-eslint/no-unused-vars": ["warn", { "varsIgnorePattern": "^_" }],
+    "@typescript-eslint/no-unused-vars": ["warn", { "varsIgnorePattern": "^_", "argsIgnorePattern": "^_" }],
     "semi": "error",
   },
 }
