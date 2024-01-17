0ab1ec6172045d466b82ba65e3fbd7243fb66695
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-17 18:37:05 +0000

    second stroke level

--- src/components/SketchCanvas.tsx
+++ src/components/SketchCanvas.tsx
@@ -60,8 +60,8 @@ const SketchCanvas = forwardRef<SketchCanvasRef, SketchCanvasProps>((props, ref)
     const cursor = useRef(new DynamicCursor({
         max_radius: max_radius,
         init_radius: props.pen_radius, // TODO: option to resize based on calculated pressure
-        stroke: "#00000080",
-        stroke_width: 1.5 // TODO: option to make size consistent with radius (also consider viewport)
+        outer_stroke: "#00000080",
+        outer_stroke_width: 1.5 // TODO: option to make size consistent with radius (also consider viewport)
         // TODO: adjust transparency based on alpha
     }));
 
--- src/util/DynamicCursor.ts
+++ src/util/DynamicCursor.ts
@@ -1,7 +1,11 @@
 export interface DynamicCursorProps {
     fill?: string;
-    stroke?: string;
-    stroke_width?: number;
+
+    outer_stroke?: string;
+    outer_stroke_width?: number;
+
+    inner_stroke?: string;
+    inner_stroke_width?: number;
 
     init_radius?: number;
     max_radius?: number;
@@ -9,18 +13,21 @@ export interface DynamicCursorProps {
 
 class DynamicCursor {
     #svg: SVGSVGElement;
-    #circle: SVGCircleElement;
+    #outer_circle: SVGCircleElement;
+    #inner_circle: SVGCircleElement;
 
     constructor(props?: DynamicCursorProps) {
         if (!props) props = {};
         if (!props.fill) props.fill = "red";
-        if (!props.stroke) props.stroke = "black";
-        if (!props.stroke_width) props.stroke_width = 1;
+        if (!props.outer_stroke) props.outer_stroke = "black";
+        if (!props.outer_stroke_width) props.outer_stroke_width = 1;
+        if (!props.inner_stroke) props.inner_stroke = "white";
+        if (!props.inner_stroke_width) props.inner_stroke_width = 1;
         if (!props.init_radius) props.init_radius = 5;
         if (!props.max_radius) props.max_radius = 20;
 
         // needs to fit the diameter of the circle and stroke
-        const svg_length = props.max_radius * 2 + props.stroke_width * 2;
+        const svg_length = props.max_radius * 2 + props.outer_stroke_width * 2;
 
         this.#svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         this.#svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
@@ -28,31 +35,46 @@ class DynamicCursor {
         this.#svg.setAttribute("width", String(svg_length));
         this.#svg.setAttribute("height", String(svg_length));
 
-        this.#circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        this.#circle.setAttribute("cx", String(svg_length / 2));
-        this.#circle.setAttribute("cy", String(svg_length / 2));
-        this.#circle.setAttribute("r", String(props.init_radius));
-        this.#circle.setAttribute("fill", props.fill);
-        this.#circle.setAttribute("stroke", props.stroke);
-        this.#circle.setAttribute("stroke-width", String(props.stroke_width));
+        this.#outer_circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        this.#outer_circle.setAttribute("cx", String(svg_length / 2));
+        this.#outer_circle.setAttribute("cy", String(svg_length / 2));
+        this.#outer_circle.setAttribute("r", String(props.init_radius));
+        this.#outer_circle.setAttribute("stroke", props.outer_stroke);
+        this.#outer_circle.setAttribute("stroke-width", String(props.outer_stroke_width));
+        this.#svg.appendChild(this.#outer_circle);
 
-        this.#svg.appendChild(this.#circle);
+        this.#inner_circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        this.#inner_circle.setAttribute("cx", String(svg_length / 2));
+        this.#inner_circle.setAttribute("cy", String(svg_length / 2));
+        this.#inner_circle.setAttribute("r", String(props.init_radius - props.inner_stroke_width));
+        this.#inner_circle.setAttribute("fill", props.fill);
+        this.#inner_circle.setAttribute("stroke", props.inner_stroke);
+        this.#inner_circle.setAttribute("stroke-width", String(props.inner_stroke_width));
+        this.#svg.appendChild(this.#inner_circle);
     }
 
     set_fill(fill: string) {
-        this.#circle.setAttribute("fill", fill);
+        this.#inner_circle.setAttribute("fill", fill);
     }
 
-    set_stroke(stroke: string) {
-        this.#circle.setAttribute("stroke", stroke);
+    set_outer_stroke(stroke: string) {
+        this.#outer_circle.setAttribute("stroke", stroke);
     }
 
-    set_stroke_width(stroke_width: number) {
-        this.#circle.setAttribute("stroke-width", String(stroke_width));
+    set_outer_stroke_width(stroke_width: number) {
+        this.#outer_circle.setAttribute("stroke-width", String(stroke_width));
+    }
+
+    set_inner_stroke(stroke: string) {
+        this.#inner_circle.setAttribute("stroke", stroke);
+    }
+
+    set_inner_stroke_width(stroke_width: number) {
+        this.#inner_circle.setAttribute("stroke-width", String(stroke_width));
     }
 
     set_radius(radius: number) {
-        this.#circle.setAttribute("r", String(radius));
+        this.#outer_circle.setAttribute("r", String(radius));
     }
 
 
@@ -68,7 +90,7 @@ class DynamicCursor {
     as_css_cursor(fallback: string = "default") {
         const data = this.as_data_url();
 
-        const center = this.#circle.getAttribute("cx") + " " + this.#circle.getAttribute("cy");
+        const center = this.#outer_circle.getAttribute("cx") + " " + this.#outer_circle.getAttribute("cy");
         const css = `url(${data}) ${center}, ${fallback}`;
 
         return css;
