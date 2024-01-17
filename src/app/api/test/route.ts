6cf95e9cd345b9494d242b7cc4201c3cd2533f8c
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-17 23:48:48 +0000

    deployment script + runtime

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,4 +1,5 @@
 export const dynamic = "force-dynamic"; // defaults to auto
+export const runtime = "edge";
 
 export async function GET(_request: Request) {
     return new Response("hello world");
