87810045e5bcca9a57f9015f7a64915e2985b9ae
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-18 01:15:05 +0000

    privacy policy (start of it)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     //output: "export", // Outputs a Single-Page Application (SPA). // disabled so api routes work. revert for cheaper deployment if we decide not to use api routes.
+
+    // use rewrites to serve html directly without boilerplate
+    rewrites: async () => [
+        {
+            source: "/privacy",
+            destination: "/html/privacy.html",
+        },
+    ],
 };
 
 export default nextConfig;
