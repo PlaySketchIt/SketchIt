0538975065265cd31f0097c919a034451a46167c
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-18 00:32:56 +0000

    change dist -> .next

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     //output: "export", // Outputs a Single-Page Application (SPA). // disabled so api routes work. revert for cheaper deployment if we decide not to use api routes.
-    distDir: "./dist", // Changes the build output directory to `./dist/`.
-    
 };
 
 export default nextConfig;
