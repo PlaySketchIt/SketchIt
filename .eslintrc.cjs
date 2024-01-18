3048cd9cf0677914d92ac831fd9358c7d500870a
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-18 00:02:59 +0000

    multiple deploy branches + build tools

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,19 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:@next/next/recommended"
+    "plugin:@next/next/recommended",
+    "plugin:next-on-pages/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: [
+    "react-refresh",
+    "next-on-pages"
+  ],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
-      { 
+      {
         allowConstantExport: true,
         allowExportNames: ["metadata", "viewport"],
       },
