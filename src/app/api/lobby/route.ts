4e5ba01ab375e8e0cfa6fd4b610fb06c80f0170f
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-24 19:32:50 +0000

    change route

--- src/app/api/lobby/route.ts
+++ src/app/api/lobby/route.ts
@@ -6,7 +6,6 @@ export const runtime = "edge";
 const bearer_token = "Bearer " + process.env.SERVER_KEY;
 
 const create_lobby_url = new URL("/create", process.env.SERVER_URL);
-const destroy_lobby_url = new URL("/destroy", process.env.SERVER_URL);
 
 // creates a new lobby
 export async function POST(_request: Request) {
@@ -29,25 +28,5 @@ export async function POST(_request: Request) {
     });
 }
 
-// destroys an existing lobby
-export async function DELETE(_request: Request) {
-    fetch(destroy_lobby_url, {
-        method: "GET",
-        headers: {
-            "Authorization": bearer_token,
-        },
-    }).then((response) => {
-        if (response.ok) {
-            return response.text();
-        } else {
-            throw new Error("HTTP status " + response.status);
-        }
-    }).then((code) => {
-        return NextResponse.json({ code });
-    }).catch((error) => {
-        console.error(error);
-        return NextResponse.error();
-    });
-}
-
 // TODO: what is the benefit of protecting the API with a secret key at the server side?
+// TODO: ^^ because we don't actually want to expose all of these methods directly, e.g. destruction is protected
