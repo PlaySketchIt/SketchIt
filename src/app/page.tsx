0c1d49b3cbf2c9f4fadd4049f794d8bdaa983b0c
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-26 00:04:13 +0000

    basic main menu layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,59 @@
+"use client";
+
+import { useState } from "react";
+
+import Link from "next/link";
+
 export default function Page() {
+    const [code, setCode] = useState("");
+    const [name, setName] = useState("");
+
+
     return (
-        <p>I wish I was a home page...</p>
+        <>
+            <label>
+                Lobby Code:
+
+                <input
+                    type="text"
+                    placeholder="Enter lobby code"
+
+                    minLength={10}
+                    maxLength={10}
+
+                    value={code}
+                    onChange={(event) => setCode(event.target.value)}
+                />
+            </label>
+
+            <label>
+                Username:
+
+                <input
+                    type="text"
+                    placeholder="Enter username"
+
+                    minLength={1}
+                    maxLength={16}
+
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                />
+            </label>
+
+            <Link href = {{
+                pathname: "/game",
+                query: {
+                    code,
+                    name,
+                }
+            }}>
+                Join
+            </Link>
+        </>
     );
 }
+
+// TODO:ux: improve ui
+// TODO:structure: sync min and max lengths with server
+
