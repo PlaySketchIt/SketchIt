c83e1723a7277d9eb973b6d12c30303346a9f484
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-28 02:06:18 +0000

    share dt component

--- src/app_components/IndexApp/index.tsx
+++ src/app_components/IndexApp/index.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-import DeferredTranslation from "./DeferredTranslation";
+import DeferredTranslation from "../../components/DeferredTranslation";
 
 const ClientApp = dynamic(() => import("./ClientApp"), { ssr: false });
 
