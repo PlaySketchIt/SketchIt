3f9be87b3d213f6c42508f1d63aff8b801d13b3f
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-01-30 23:53:21 +0000

    use inferred env vars

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -5,11 +5,11 @@ export const {
     handlers: { GET, POST },
     auth,
 } = NextAuth({
-    providers: [Discord({
-        clientId: process.env.DISCORD_CLIENT_ID,
-        clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    })],
+    providers: [Discord],
 });
 
 // auth from client components using useSession, as sessionprovider is provided to all pages by page.tsx
 // auth from server components uses the auth() function exported from this file
+
+// don't forget to set AUTH_DISCORD_ID and AUTH_DISCORD_SECRET in the environment
+// also set NEXTAUTH_SECRET (always) and NEXTAUTH_URL (in prod, same as BASE_URL)
